7bc39e91fec9aaf1ece61c150bbe708ab6b54c33
syllogismos <anilkaraka@live.in>
2017-11-24 17:15:07 +0530

    wait until filebeat to start before starting server

--- server.sh
+++ server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# wait until filebeat is started
+until pids=$(pidof filebeat)
+do   
+    sleep 5
+    echo "waiting for filebeat to start"
+done
+
 export ESCHERNODE_ENV=prod
 gunicorn -w 3 eschernode.wsgi
