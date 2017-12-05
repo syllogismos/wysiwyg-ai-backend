c44920d3370ca41c3d67f03c18b280dea0a14121
syllogismos <anilkaraka@live.in>
2017-12-05 21:01:26 +0530

    new ami

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-RLLAB_AMI = "ami-c6f294bc"
+RLLAB_AMI = "ami-f8fb9d82"
 
 if os.environ['ESCHERNODE_ENV'] == 'prod':
     USER_DATA = open('/home/ubuntu/dashboard_backend/core/rl_user_data_script.sh', 'rb').read()
